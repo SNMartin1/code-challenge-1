4afe2e1fb4a8e47ca569da3920a6d7db9d845092
Nicole Martin <nikismar@hotmail.com>
2017-06-26 14:34:03 -0500

    appends red divs on DOM

--- client.js
+++ client.js
@@ -1,10 +1,33 @@
 console.log('js sourced');
+var count = 1;
 
-$(document).ready(function (){
-  $('.generate').on('click', function(){
+$(document).ready(function(){
+
+  $('.generate').on('click', createRedDiv);
+  $('#swap').on('click', swapcolor);
+
+}); //end of ready function
+
+//  Need to add code to add to the count in added divs
+function createRedDiv() {
+    $('.redDiv').empty();
     $('.container').append('<div class="redDiv"></div>');
-    $('.redDiv').append('<p>1</p>');
-    $('.redDiv').append('<button>swap</button>');
-    $('.redDiv').append('<button>delete</button>');
-  });
-});
+    //$('p').empty();
+    $('.redDiv').append('<p> ' + count + ' </p>');
+    //$('p').append(count +1); 
+    $('.redDiv').append('<button id="swap">swap</button>');
+    $('.redDiv').append('<button id="delete">delete</button>');
+} // end of createRedDiv function
+
+//swap color from red to yellow
+function swapColor() {
+    $(this).css('background-color', 'yellow');
+    //$('.redDiv').css('backgroung-color', yellow);
+
+}
+
+
+//delete div and all it's contents
+function divDelete() {
+  $('.redDiv').remove();
+}
